f336ca0df6bc56843cf3cf74b9a4577795088aa3
Liwei0503 <liwei0503@gmail.com>
2019-10-11 04:02:48 +0800

    Update Install_take_photos_tools.sh

--- Install_take_photos_tools.sh
+++ Install_take_photos_tools.sh
@@ -7,3 +7,21 @@ sudo apt-get install imagemagick
 # fswebcam -d /dev/video0 -r 1920x1080 -S 30 -F 5 image10.jpg
 # example 2.:
 # fswebcam -d /dev/video0 --no-banner -r 640x480 -S 29 image11.jpg
+#-----------------------------------------------------------------------
+
+sudo apt-get update
+sudo apt-get install subversion
+sudo apt-get install libjpeg8-dev
+sudo apt-get install imagemagick
+sudo apt-get install libv4l-dev
+sudo apt-get install cmake
+sudo apt-get install git
+sudo apt-get install uv4l uv4l-raspicam
+
+#------------------------------------------------------------------------
+sudo pkill uv4l
+sudo apt-get update
+sudo apt-get install uv4l-uvc
+sudo apt-get install uv4l-xscreen
+sudo apt-get install uv4l-mjpegstream
+
