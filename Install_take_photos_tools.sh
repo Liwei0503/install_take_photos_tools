8f3d830d5909f84f52b12fd72c365e384ee5abc1
Liwei0503 <liwei0503@gmail.com>
2019-10-11 03:55:36 +0800

    Update Install_take_photos_tools.sh

--- Install_take_photos_tools.sh
+++ Install_take_photos_tools.sh
@@ -3,5 +3,7 @@ sudo apt-get upgrate
 sudo apt-get -y install fswebcam bison v4l-utils motion
 sudo apt-get install libjpeg8-dev
 sudo apt-get install imagemagick
-# example:
+# example 1.:
 # fswebcam -d /dev/video0 -r 1920x1080 -S 30 -F 5 image10.jpg
+# example 2.:
+# fswebcam -d /dev/video0 --no-banner -r 640x480 -S 29 image11.jpg
